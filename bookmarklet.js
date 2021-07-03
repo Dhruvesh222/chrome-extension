a42e1d9c898ea07f442929bb256bd4c2eeeabf6e
Dhruvesh Bhure <84266782+Dhruvesh222@users.noreply.github.com>
2021-07-03 12:19:31 +0530

    Update bookmarklet.js
    changes allp

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 (function () {
-    var allp = document.getElementsByClassName('cbox')
+    var allp = document.getElementsByTagName('p');
     for (let i = 0; i < allp.length; i++) {
         allp[i].style.backgroundColor = 'red';
     }
